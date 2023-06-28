21150d3b74123b32f39f7eb537aacf9abe23f9df
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-06-28 10:54:55 +0200

    Respond to all connections

--- backend/src/main/java/com/example/backend/ChatService.java
+++ backend/src/main/java/com/example/backend/ChatService.java
@@ -3,35 +3,45 @@ package com.example.backend;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class ChatService extends TextWebSocketHandler {
 
     private final List<String> history = new ArrayList<>();
+    private final Set<WebSocketSession> sessions = new HashSet<>();
 
     private final ObjectMapper objectMapper;
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-
         String json = objectMapper.writeValueAsString(history);
-
         session.sendMessage(new TextMessage(json));
+        sessions.add(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
         String payload = message.getPayload();
         history.add(payload);
-        System.out.println(payload);
-        session.sendMessage(new TextMessage("Danke für Deine Nachricht '" + payload + "' - bis zum nächsten Mal."));
+
+        for (WebSocketSession otherSession : sessions) {
+            otherSession.sendMessage(new TextMessage(payload));
+        }
     }
 }
