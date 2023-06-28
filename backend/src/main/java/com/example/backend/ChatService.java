0a2e4be7fc7c8c99883b135a3510765d419c1db8
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-06-28 10:45:30 +0200

    Store history of messages in backend

--- backend/src/main/java/com/example/backend/ChatService.java
+++ backend/src/main/java/com/example/backend/ChatService.java
@@ -6,13 +6,24 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ChatService extends TextWebSocketHandler {
 
+    private final List<String> history = new ArrayList<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        session.sendMessage(new TextMessage("Bisherige Meldungen:" + history));
+    }
+
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
-        System.out.println(message.getPayload());
-        session.sendMessage(new TextMessage("Danke für Deine Nachricht '"+message.getPayload()+"' - bis zum nächsten Mal."));
+        String payload = message.getPayload();
+        history.add(payload);
+        System.out.println(payload);
+        session.sendMessage(new TextMessage("Danke für Deine Nachricht '" + payload + "' - bis zum nächsten Mal."));
     }
 }
